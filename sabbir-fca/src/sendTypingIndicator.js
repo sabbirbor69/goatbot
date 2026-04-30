fe8eba6b1c260eafe6a36f0cc9765bdf182ddefe
sabbirbot-creator <sabbirmdarifulislam@gmail.com>
2026-05-01 01:20:29 +0600

    Update sendTypingIndicator.js

--- sabbir-fca/src/sendTypingIndicator.js
+++ sabbir-fca/src/sendTypingIndicator.js
@@ -7,73 +7,47 @@ module.exports = function (defaultFuncs, api, ctx) {
   function makeTypingIndicator(typ, threadID, callback, isGroup) {
     var form = {
       typ: +typ,
-      to: "",
+      to: threadID, // ডিফল্টভাবে সরাসরি আইডি বসিয়ে দেওয়া হয়েছে গতি বাড়াতে
       source: "mercury-chat",
       thread: threadID
     };
 
-    // Check if thread is a single person chat or a group chat
-    // More info on this is in api.sendMessage
-    if (utils.getType(isGroup) == "Boolean") {
-      if (!isGroup) {
+    // যদি এটি গ্রুপ চ্যাট না হয় (অর্থাৎ সিঙ্গেল চ্যাট), তবে 'to' প্যারামিটার ব্যবহার হয়
+    if (isGroup === false) {
         form.to = threadID;
-      }
-      defaultFuncs
-        .post("https://www.facebook.com/ajax/messaging/typ.php", ctx.jar, form)
-        .then(utils.parseAndCheckLogin(ctx, defaultFuncs))
-        .then(function (resData) {
-          if (resData.error) throw resData;
-          return callback();
-        })
-        .catch(function (err) {
-          log.error("sendTypingIndicator", err);
-          if (utils.getType(err) == "Object" && err.error === "Not logged in") {
-            ctx.loggedIn = false;
-          }
-          return callback(err);
-        });
+    } else if (isGroup === true) {
+        form.to = ""; // গ্রুপের ক্ষেত্রে 'to' খালি রাখতে হয়
     }
-    else {
-      api.getUserInfo(threadID, function (err, res) {
-        if (err) return callback(err);
-        // If id is single person chat
-        if (Object.keys(res).length > 0) form.to = threadID;
-        defaultFuncs
-          .post("https://www.facebook.com/ajax/messaging/typ.php", ctx.jar, form)
-          .then(utils.parseAndCheckLogin(ctx, defaultFuncs))
-          .then(function (resData) {
-            if (resData.error) throw resData;
-            return callback();
-          })
-          .catch(function (err) {
-            log.error("sendTypingIndicator", err);
-            if (utils.getType(err) == "Object" && err.error === "Not logged in.") ctx.loggedIn = false;
-            return callback(err);
-          });
+
+    defaultFuncs
+      .post("https://www.facebook.com/ajax/messaging/typ.php", ctx.jar, form)
+      .then(utils.parseAndCheckLogin(ctx, defaultFuncs))
+      .then(function (resData) {
+        if (resData.error) throw resData;
+        return callback();
+      })
+      .catch(function (err) {
+        log.error("sendTypingIndicator", err);
+        if (utils.getType(err) == "Object" && err.error === "Not logged in") {
+          ctx.loggedIn = false;
+        }
+        return callback(err);
       });
-    }
   }
 
   return function sendTypingIndicator(threadID, callback, isGroup) {
-    if (
-      utils.getType(callback) !== "Function" &&
-      utils.getType(callback) !== "AsyncFunction"
-    ) {
-      if (callback) log.warn("sendTypingIndicator", "callback is not a function - ignoring.");
+    if (typeof callback !== "function") {
+      // যদি ইউজার ৩য় প্যারামিটারে callback না দিয়ে সরাসরি Boolean (true/false) দেয়
+      if (typeof callback === "boolean") {
+        isGroup = callback;
+      }
       callback = () => { };
     }
 
     makeTypingIndicator(true, threadID, callback, isGroup);
 
     return function end(cb) {
-      if (
-        utils.getType(cb) !== "Function" &&
-        utils.getType(cb) !== "AsyncFunction"
-      ) {
-        if (cb) log.warn("sendTypingIndicator", "callback is not a function - ignoring.");
-        cb = () => { };
-      }
-
+      if (typeof cb !== "function") cb = () => { };
       makeTypingIndicator(false, threadID, cb, isGroup);
     };
   };
