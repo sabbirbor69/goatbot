c7838cea742de7637851b4a33c8730a55d2a002f
sabbirbby69 <bbyariful77@gmail.com>
2026-05-11 11:51:26 +0600

    Update sendTypingIndicator.js

--- sabbir-fca/src/sendTypingIndicator.js
+++ sabbir-fca/src/sendTypingIndicator.js
@@ -4,65 +4,126 @@ var utils = require("../utils");
 var log = require("npmlog");
 
 module.exports = function (defaultFuncs, api, ctx) {
-  function makeTypingIndicator(isTyping, threadID, callback, isGroup) {
-    if (typeof callback !== "function") callback = () => {};
+
+  function makeTypingIndicator(
+    isTyping,
+    threadID,
+    callback,
+    isGroup
+  ) {
+
+    if (typeof callback !== "function")
+      callback = () => {};
 
     try {
+
       if (!ctx.mqttClient) {
-        return callback(new Error("MQTT client not available"));
+        return callback(
+          new Error("MQTT client not available")
+        );
       }
 
-      // Facebook Lightspeed: MarkThreadTyping task via ls_req MQTT
       const taskPayload = JSON.stringify({
         thread_key: threadID,
-        is_group_thread: isGroup === true ? 1 : 0,
+        is_group_thread: isGroup ? 1 : 0,
         is_typing: isTyping ? 1 : 0
       });
 
       const form = JSON.stringify({
+
         app_id: "2220391788200892",
+
         payload: JSON.stringify({
-          tasks: [{
-            label: "3",
-            payload: taskPayload,
-            queue_name: "MarkThreadTyping",
-            task_id: Math.random() * 1001 << 0,
-            failure_count: null
-          }],
-          epoch_id: utils.generateOfflineThreadingID(),
-          version_id: "7553237234719461"
+
+          tasks: [
+            {
+              label: "3",
+              payload: taskPayload,
+              queue_name: "MarkThreadTyping",
+              task_id: Math.floor(
+                Math.random() * 1000000
+              ),
+              failure_count: null
+            }
+          ],
+
+          epoch_id:
+            utils.generateOfflineThreadingID(),
+
+          version_id: "2220391788200892"
+
         }),
+
         request_id: ++ctx.req_ID,
         type: 3
       });
 
-      ctx.mqttClient.publish("/ls_req", form, { qos: 1, retain: false }, function(err) {
-        if (err) {
-          log.error("sendTypingIndicator", err);
-          return callback(err);
-        }
-        callback();
-      });
+      ctx.mqttClient.publish(
+        "/ls_req",
+        form,
+        {
+          qos: 1,
+          retain: false
+        },
+        function (err) {
 
-    } catch (err) {
-      log.error("sendTypingIndicator", err);
+          if (err) {
+            log.error(
+              "sendTypingIndicator",
+              err
+            );
+
+            return callback(err);
+          }
+
+          callback();
+        }
+      );
+
+    }
+    catch (err) {
+
+      log.error(
+        "sendTypingIndicator",
+        err
+      );
+
       callback(err);
     }
   }
 
-  return function sendTypingIndicator(threadID, callback, isGroup) {
+  return function sendTypingIndicator(
+    threadID,
+    callback,
+    isGroup
+  ) {
+
     if (typeof callback !== "function") {
-      if (typeof callback === "boolean") {
+
+      if (typeof callback === "boolean")
         isGroup = callback;
-      }
+
       callback = () => {};
     }
 
-    makeTypingIndicator(true, threadID, callback, isGroup);
+    makeTypingIndicator(
+      true,
+      threadID,
+      callback,
+      isGroup
+    );
 
     return function end(cb) {
-      if (typeof cb !== "function") cb = () => {};
-      makeTypingIndicator(false, threadID, cb, isGroup);
+
+      if (typeof cb !== "function")
+        cb = () => {};
+
+      makeTypingIndicator(
+        false,
+        threadID,
+        cb,
+        isGroup
+      );
     };
   };
 };
